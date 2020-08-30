8a664fbabc748ef95c9b453540432d138abb512a
Michael Cook <mcook0775@gmail.com>
2020-08-30 13:53:07 -0400

    Add authorization module

---
+++ middleware/authorization.js
@@ -0,0 +1,33 @@
+// run on protected routes
+
+const auth = require("basic-auth")
+const bcrypt = require("bcryptjs")
+const UserService = require("../services/UserService")
+
+module.exports = () => {
+  /**
+   * Parse the HTTP Authorization Header
+   * On success call next in middleware chain
+   * On fail return 401 status with a message
+   */
+  return async function (req, res, next) {
+    let message
+    const credentials = auth(req)
+    if (credentials) {
+      const { name, pass } = credentials
+      const user = await UserService.getUser()
+      if (user) {
+        const authed = bcrypt.compareSync(pass, user.password)
+        if (authed) {
+          req.user = user
+          next()
+        }
+      } else {
+        message = "User not found."
+      }
+    } else {
+      message = "Incorrect email or password."
+    }
+    return res.status(401).json({ message })
+  }
+}
--- services/UserService.js
+++ services/UserService.js
@@ -2,6 +2,20 @@ const { User } = require("../models/index")
 
 module.exports = class UserService {
   /**
+   * Return object for owner of site
+   */
+  static async getUser() {
+    try {
+      await User.sync()
+      const user = await User.findByPk(1)
+      return user
+    } catch (error) {
+      console.log(error)
+      return false
+    }
+  }
+
+  /**
    * Update table cols specified in payload
    * @param {Object} payload - object containing properties to change
    * @return {Promise} true on success, false on fail
