c8bf6dded535ee067af3bde05e446455a48b13e7
Michael A. Cook <mcook0775@gmail.com>
2020-09-20 12:45:28 -0400

    Add CORS

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const routes = require("./routes/index")
 
 const app = express()
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
