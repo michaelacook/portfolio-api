3a675badea20ead671914fd6f430d75123d1462b
Michael A. Cook <mcook0775@gmail.com>
2020-09-25 18:03:04 -0400

    Remove show stack trace on 500 error

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use((err, req, res, next) => {
   res.locals.error = req.app.get("env") === "development" ? err : {}
 
   // render the error page
-  res.status(err.status || 500).json(err.stack)
+  res.status(err.status || 500).json(err)
 })
 
 module.exports = app
