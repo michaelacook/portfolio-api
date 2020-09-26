b9c740f4fc8e70a5337ec58197b7adf0239f1617
Michael A. Cook <mcook0775@gmail.com>
2020-09-26 17:49:01 -0400

    Add cors preflight

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const routes = require("./routes/index")
 const app = express()
 
 app.use(cors())
+app.options('*', cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
