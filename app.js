76a97dcab22c8c582b3e8a837c2923af168e59e6
Michael A. Cook <mcook0775@gmail.com>
2021-04-16 19:22:00 -0400

    Set up env var loading

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
+
 const createError = require("http-errors")
 const express = require("express")
 const cookieParser = require("cookie-parser")
