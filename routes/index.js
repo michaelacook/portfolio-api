7faeee6b608e13d6cacd840e9e1248796ca11d8e
Michael A. Cook <mcook0775@gmail.com>
2021-04-16 17:02:00 -0400

    Remove separate controllers to reduce complexity, remove upload code

--- routes/index.js
+++ routes/index.js
@@ -1,90 +1,134 @@
 const express = require("express")
 const router = express.Router()
 
-const multer = require("multer")
-const path = require("path")
-const storage = multer.diskStorage({
-  destination: "./public/images/",
-  filename: (req, file, cb) => {
-    cb(null, `${file.originalname}`)
-  },
-})
-
-const upload = multer({ storage }).single("img")
-
-const UserController = require("../controllers/UserController")
-const PostController = require("../controllers/PostController")
-const ProjectController = require("../controllers/ProjectController")
+const UserService = require("../services/UserService")
+const PostService = require("../services/PostService")
+const ProjectService = require("../services/ProjectService")
 const authorize = require("../middleware/authorization")()
 
 // User routes------------------------------------------------//
 
 // private
-router.get("/user", authorize, (req, res, next) => {
-  UserController.userGET(req, res, next)
+router.get("/user", authorize, async (req, res, next) => {
+  try {
+    const email = req.user.email
+    const user = await UserService.getUser(email)
+    return res.json(user)
+  } catch (error) {
+    next(error)
+  }
 })
 
 // Post routes------------------------------------------------//
 
-router.get("/posts", (req, res, next) => {
-  PostController.postsGET(req, res, next)
+router.get("/posts", async (req, res, next) => {
+  try {
+    const posts = await PostService.getAllPosts()
+    return res.json(posts)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.get("/posts/:id", (req, res, next) => {
-  PostController.postGET(req, res, next)
+router.get("/posts/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const post = await PostService.getPost(id)
+    return res.json(post)
+  } catch (error) {
+    next(error)
+  }
 })
 
 // private route
-router.post("/posts/add", authorize, (req, res, next) => {
-  PostController.postPOST(req, res, next)
+router.post("/posts/add", authorize, async (req, res, next) => {
+  try {
+    const id = await PostService.createPost(req.body)
+    return res.status(201).json(id)
+  } catch (error) {
+    next(error)
+  }
 })
 
 // private route
-router.put("/posts/:id/update", authorize, (req, res, next) => {
-  PostController.postPUT(req, res, next)
+router.put("/posts/:id/update", authorize, async (req, res, next) => {
+  try {
+    const id = req.params.id
+    await PostService.updatePost(id, req)
+    return res.status(204).end()
+  } catch (error) {
+    next(error)
+  }
 })
 
 // private route
-router.delete("/posts/:id/delete", authorize, (req, res, next) => {
-  PostController.postDELETE(req, res, next)
+router.delete("/posts/:id/delete", authorize, async (req, res, next) => {
+  try {
+    const id = req.params.id
+    await PostService.deletePost(id)
+    return res.status(204).end()
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.get("/posts/search/:keyword", (req, res, next) => {
-  PostController.postSEARCH(req, res, next)
+router.get("/posts/search/:keyword", async (req, res, next) => {
+  try {
+    const keyword = req.params.keyword
+    const posts = await PostService.searchPost(keyword)
+    return res.json(posts)
+  } catch (error) {
+    next(error)
+  }
 })
 
 // Project routes---------------------------------------------//
-router.get("/projects", (req, res, next) => {
-  ProjectController.projectsGET(req, res, next)
+router.get("/projects", async (req, res, next) => {
+  try {
+    const projects = await ProjectService.getProjects()
+    return res.json(projects)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.get("/projects/:id", (req, res, next) => {
-  ProjectController.projectGET(req, res, next)
+router.get("/projects/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const project = await ProjectService.getProject(id)
+    return res.json(project)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.post("/projects/add", authorize, (req, res, next) => {
-  console.log(req.body)
-  ProjectController.projectPOST(req, res, next)
+router.post("/projects/add", authorize, async (req, res, next) => {
+  try {
+    await ProjectService.addProject(req)
+    return res.status(201).end()
+  } catch (error) {
+    next(error)
+  }
 })
 
-// handle file upload
-// make this a protected route
-router.post("/projects/upload", (req, res) => {
-  upload(req, res, (err) => {
-    if (err) {
-      console.log(err)
-      return res.status(500).end()
-    }
-    res.status(204).end()
-  })
+router.put("/projects/:id/update", authorize, async (req, res, next) => {
+  try {
+    const id = req.params.id
+    await ProjectService.updateProject(id, req.body)
+    return res.status(204).end()
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.put("/projects/:id/update", authorize, (req, res, next) => {
-  ProjectController.projectPUT(req, res, next)
-})
-
-router.delete("/projects/:id/delete", authorize, (req, res, next) => {
-  ProjectController.projectDELETE(req, res, next)
+router.delete("/projects/:id/delete", authorize, async (req, res, next) => {
+  try {
+    const id = req.params.id
+    await ProjectService.deleteProject(id)
+    return res.status(204).end()
+  } catch (error) {
+    next(error)
+  }
 })
 
 // User routes------------------------------------------------//
